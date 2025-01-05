f5acb5a5d079973bede5a6eb39bf7d6cb3d171ec
siranjeevi <ksiranjeevi92@gmail.com>
2025-01-05 12:08:49 +0530

    Connected to DB

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # recipe-app-api
+
 Django REST API
+
+docker compose down --rmi all --volumes --remove-orphans
+
+docker compose build
+
+docker compose run --rm app sh -c "pip list"
--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -1,5 +1,23 @@
 from django.core.management.base import BaseCommand
+from django.db.utils import OperationalError
+
+from psycopg2 import OperationalError as Psycopg2OpError
+
+import time
+
+#from pyscopg2 import OperationError as Psycopg2OpError
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        return super().handle(*args, **options)
+        self.stdout.write("waiting for database")
+        db_up = False
+        while db_up is False:
+            try:
+                self.check(databases=['default'])
+                db_up = True
+            except (OperationalError, Psycopg2OpError):
+                self.stdout.write("Databse unavailable! waiting for 1 second")
+                time.sleep(1)
+        self.stdout.write(self.style.SUCCESS("Database available!"))
+
+        
