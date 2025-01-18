2620ebda5858046f52fb415872c8403188b6cc85
siranjeevi <ksiranjeevi92@gmail.com>
2025-01-18 15:03:52 +0530

    git add .

---
+++ app/user/views.py
@@ -0,0 +1,8 @@
+"""View for user API"""
+from rest_framework import generics
+
+from user.serializers import UserSerializer
+
+
+class CreateUserView(generics.CreateAPIView):
+    serializer_class = UserSerializer
